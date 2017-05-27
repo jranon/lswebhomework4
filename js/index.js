a026724e123910ff9153536c10c4a896614723c3
Jesse <jesse.ranon@gmail.com>
2017-05-27 14:22:07 -0700

    Completed extra credit exercises.

--- js/index.js
+++ js/index.js
@@ -9,3 +9,21 @@ document.getElementById("nickname").innerHTML="Jay";
 document.getElementById("favorites").innerHTML="49ers, Sushi, Poetry, Weightlifting";
 
 document.getElementById("hometown").innerHTML="Honolulu, HI";
+
+var lis = document.querySelectorAll("li");
+
+for (var i=0; i<lis.length; i++) {
+  lis[i].className="listitem";
+}
+
+var listitems = document.getElementsByClassName("listitem");
+
+for (var i=0; i<listitems.length; i++) {
+  listitems[i].style.color="red";
+}
+
+var mypic = document.createElement("img")
+
+mypic.setAttribute('src', 'http://i.imgur.com/9uilccQ.jpg');
+
+document.body.appendChild(mypic);
