3aa5014a77d609bfc5a644ce75ee7868856a0379
Jesse <jesse.ranon@gmail.com>
2017-05-27 13:40:41 -0700

    Committing main assignment.  Extra credit to follow.

---
+++ js/index.js
@@ -0,0 +1,11 @@
+document.body.style.backgroundColor="red";
+
+document.querySelector("h1").style.backgroundColor="blue";
+
+document.body.style.fontFamily="sans-serif";
+
+document.getElementById("nickname").innerHTML="Jay";
+
+document.getElementById("favorites").innerHTML="49ers, Sushi, Poetry, Weightlifting";
+
+document.getElementById("hometown").innerHTML="Honolulu, HI";
